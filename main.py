46d67bb533bb1273a6c42e3dd54276a649c0e01c
Nazeef1 <nazeefrafiqui@gmail.com>
2022-07-15 08:43:20 +0400

    smol

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from tabulate import tabulate
+import datetime
 import mysql.connector as mysql
 conn=mysql.connect(host='localhost', user='root', password='nazeef')
 cs=conn.cursor()
 cs.execute("create database if not exists horsepower")
 cs.execute("use horsepower")
 cs.execute('create table if not exists Member_Info(id int(10) primary key not null,Member_Name varchar(30),age int,gender varchar(6),mobile int,Activities varchar(40),password varchar(20) invisible)')
-cs.execute('create table if not exists Member_package_info(id int(10),Member_Name varchar(30),Activities varchar(40),special_package varchar(40),total_payment int,monthly_payment int,end_of_membership date,constraint foreign key(id,Member_Name,Activities) references Member_Info(id,Member_Name,Activities))')
+cs.execute("create table if not exists Member_package_info(id int(10),Member_Name varchar(30),Activities varchar(40),special_package varchar(40),total_payment int,monthly_payment int,end_of_membership date,constraint foreign key(id,Member_Name,Activities) references Member_Info(id,Member_Name,Activities))")
 class bgc:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -89,8 +90,7 @@ elif login == 'member':
     while True:
         print('''MENU:
         1.Login.
-        2.View your details.
-        3.Exit.'''.format(bgc.BOLD))
+        2.Exit.'''.format(bgc.BOLD))
         ch = int(input('Enter choice 1/2/3:'))
         if ch == 1:
             mobile = input('Enter mobile no.: ')
@@ -101,6 +101,41 @@ elif login == 'member':
             else:
                 print('Invalid mobile number or password.')
                 continue
-
+            print('''What would you like to do?
+            1.Insert details in your progress tracker.
+            2.View your progress tracker.
+            3.View your package details.
+            4.EXIT.'''.format(bgc.BOLD))
+            ch = int(input('Enter choice 1/2/3/4:'))
+            if ch == 1:
+                cs.execute('create table if not exists progress_tracker(date text,weight int,height int,weight*height**2 as bmi int, workouts varchar(20),weight_loss int,weight_gain int)')
+                date = input('Enter date: ')
+                weight = int(input('Enter weight: '))
+                height = int(input('Enter height: '))
+                workouts = input('Enter workouts: ')
+                weight_loss = int(input('Enter weight loss: '))
+                weight_gain = int(input('Enter weight gain: '))
+                bmi = weight * height ** 2
+                cs.execute("insert into progress_tracker values('%s',%s,%s,%s,'%s',%s,%s)"%(date,weight,height,bmi,workouts,weight_loss,weight_gain))
+                conn.commit()
+                print(bgc.OKGREEN + 'Details Inserted')
+            elif ch == 2:
+                cs.execute('select * from progress_tracker')
+                print(tabulate(cs.fetchall(), headers=['date','weight','height','bmi','workouts','weight_loss','weight_gain'], tablefmt='fancy_grid'))
+            elif ch == 3:
+                cs.execute('select * from member_package_info where name="%s"'%cs.execute("select member_name from member_info where mobile=%s and password='%s'"%(mobile,password)).fetchone()[1])
+                print(tabulate(cs.fetchall(), headers=['id','name','Activities','special_package','total_payment','monthly_payment','end_of_membership'], tablefmt='fancy_grid'))
+                today = datetime.datetime.now()
+                end_date = datetime.datetime(int(cs.execute("select end_of_membership from member_package_info where name='%s'"%cs.execute("select member_name from member_info where mobile=%s and password='%s'"%(mobile,password)).fetchone()[1]).fetchone()[0][0:4]),int(cs.execute("select end_of_membership from member_package_info where name='%s'"%cs.execute("select member_name from member_info where mobile=%s and password='%s'"%(mobile,password)).fetchone()[1]).fetchone()[0][5:7]),int(cs.execute("select end_of_membership from member_package_info where name='%s'"%cs.execute("select member_name from member_info where mobile=%s and password='%s'"%(mobile,password)).fetchone()[1]).fetchone()[0][8:10]))
+                difference = end_date.date() - today.date()
+                print(difference.days, 'days remaining for your membership.')
+                if difference.days <7:
+                    print('Your membership is about to expire.'
+                          'Please renew your membership at the fitness club counter.')
+                elif difference.days <= 0:
+                    print('Your membership has expired.')
+            else:
+                conn.close()
+                break
 else:
     pass
